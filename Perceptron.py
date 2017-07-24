589999abbde02773807de79e1d5cdda8be22922f
Ehshan Veerabangsa <ehshan@msn.com>
2017-07-24 16:15:16 +0100

    create nodes values and set of 4 input patterns

--- Perceptron.py
+++ Perceptron.py
@@ -1,1 +1,13 @@
 import tensorflow as tf
+
+# values for nodes
+T, F = 1., -1.
+bias = 1.
+
+# Training Set of 4 inputs
+train_in = [
+    [T, T, bias],
+    [T, F, bias],
+    [F, T, bias],
+    [F, F, bias],
+]
