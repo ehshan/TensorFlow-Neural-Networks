3e5cec4bb038d995afbcac7a1abb294e88cf27fc
Ehshan Veerabangsa <ehshan@msn.com>
2017-07-24 21:17:35 +0100

    updated format

--- Perceptron.py
+++ Perceptron.py
@@ -5,7 +5,7 @@ T, F = 1., -1.
 bias = 1.
 
 # Training Set of 4 inputs
-train_in = [
+training_in = [
     [T, T, bias],
     [T, F, bias],
     [F, T, bias],
@@ -13,7 +13,7 @@ train_in = [
 ]
 
 # Output patterns for input set
-train_out = [
+training_out = [
     [T],
     [F],
     [F],
@@ -22,31 +22,33 @@ train_out = [
 
 # Weights (3 x 1 tensor to match input patterns)
 # Values randomly initialised
-w = tf.Variable(tf.random_normal([3, 1]))
+weight = tf.Variable(tf.random_normal([3, 1]))
+
 
 # Step Activation Function
 # If x > 0 return 1 else return -1
 def step(x):
     is_greater = tf.greater(x, 0)
-    as_float = tf.to_float(is_greater)
-    double = tf.multiply(as_float, 2)
-    return tf.subtract(double, 1)
+    to_float = tf.to_float(is_greater)
+    to_double = tf.multiply(to_float, 2)
+    return tf.subtract(to_double, 1)
+
 
 # Output function
-output = step(tf.matmul(train_in, w))
+output = step(tf.matmul(training_in, weight))
 
 # Error function
-error = tf.subtract(train_out, output)
+error = tf.subtract(training_out, output)
 
 # Mean Square Error
 mse = tf.reduce_mean(tf.square(error))
 
 # Calculating the weight adjustment determined by error
-delta = tf.matmul(train_in, error, transpose_a=True)
+delta = tf.matmul(training_in, error, transpose_a=True)
 
 # Assignment the adjustment value to the weight tensor
-train = tf.assign(w, tf.add(w, delta))
+train = tf.assign(weight, tf.add(weight, delta))
 
 # Create a TensorFlow session
-sess = tf.Session()
-sess.run(tf.initialize_all_variables())
+session = tf.Session()
+session.run(tf.initialize_all_variables())
