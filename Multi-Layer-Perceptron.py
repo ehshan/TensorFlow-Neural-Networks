63c42ea403d12ed3b5bb70bf54e4f04b988ebabb
Ehshan Veerabangsa <ehshan@msn.com>
2017-07-25 19:26:33 +0100

    added intial node value + training input & output

--- Multi-Layer-Perceptron.py
+++ Multi-Layer-Perceptron.py
@@ -1,1 +1,19 @@
 import tensorflow as tf
+
+# values for nodes
+T, F = 1., -1.
+
+# Training Set of 4 input patterns
+training_in = [
+    [T, T],
+    [T, F],
+    [F, T],
+    [F, F], ]
+
+# Desired output patterns for an XOR
+training_out = [
+    [F],
+    [T],
+    [T],
+    [F],
+]
