f16dee02f071b74a27627b707feaa48d485d43ee
Ehshan Veerabangsa <ehshan@msn.com>
2017-07-25 20:55:05 +0100

    added function for sigmoid activation

--- Multi-Layer-Perceptron.py
+++ Multi-Layer-Perceptron.py
@@ -18,7 +18,6 @@ training_out = [
     [F],
 ]
 
-
 # Weights and biases for input layer
 weight_1 = tf.Variable(tf.random_normal([2, 2]))
 bias_1 = tf.Variable(tf.zeros([2]))
@@ -26,3 +25,8 @@ bias_1 = tf.Variable(tf.zeros([2]))
 # Weights and biases for hidden layer
 weight_2 = tf.Variable(tf.random_normal([2, 1]))
 bias_2 = tf.Variable(tf.zeros([1]))
+
+
+# Sigmoid Activation Function
+def sigmoid(x):
+    return 1 / (1 + tf.exp(-x))
