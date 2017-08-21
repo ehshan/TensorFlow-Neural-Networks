940641f114044bdb6eef1a806d24260e18279c75
Ehshan Veerabangsa <ehshan@msn.com>
2017-08-21 18:00:05 +0100

    added data visualisation

--- Multi-Layer-Neural-Network.py
+++ Multi-Layer-Neural-Network.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # generate sample data
 np.random.seed(1)
@@ -15,7 +16,7 @@ c1_cov = [[2, .7], [.7, 2]]
 c2_cov = [[1, .7], [.7, 1]]
 c3_cov = [[0, .7], [.7, 0]]
 
-# generate
+# GENERATE DATA
 # Cluster 1
 c1 = np.random.multivariate_normal(c1_mean, c1_cov, data_points)
 # Cluster 2
@@ -29,3 +30,10 @@ data_features = np.vstack((c1, c2, c3)).astype(np.float32)
 # Even distribution of labels -> 0, 1, 2
 data_labels = np.hstack((np.zeros(data_points), np.ones(data_points), np.ones(data_points) + 1))
 
+# VISUALISATION
+# Set the graph size
+plt.figure(figsize=(12, 8))
+# Generate distribution of data points
+plt.scatter(data_features[:, 0], data_features[:, 1], c=data_labels, alpha=.5)
+# Show graph
+plt.show()
